a6f6439ed63a70cdc767f9a54452ddbd82a2375e
test <test@test.com>
2020-04-23 11:53:59 +0000

    test

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 cd `dirname $0`
 
+apt-get install -y apt-get
+
 wget -c https://repo.continuum.io/archive/Anaconda3-2019.10-Linux-x86_64.sh
 chmod +x Anaconda3-2019.10-Linux-x86_64.sh
 bash Anaconda3-2019.10-Linux-x86_64.sh -b -f -p /usr/local
