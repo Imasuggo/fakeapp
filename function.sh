a90dd7673562e0381d859843a1cb444a4f27b9e7
test <test@test.com>
2020-04-23 12:58:47 +0000

    test

--- function.sh
+++ function.sh
@@ -3,8 +3,9 @@
 readonly EXT='mp4'
 readonly FPS='25'
 readonly TMP_DIR='tmp'
+readonly TMP_IMAGES_DIR='tmp_images'
 
-function extract_images() {
+function extract_images_from_videos() {
 
   local target_dir=$1
   local output_dir=$2
@@ -31,4 +32,25 @@ function extract_images() {
       -vf fps="${FPS}" \
       "${output_dir}/$(basename $f ".${EXT}")_%06d.png"
   done
+
+  rm -rf "{TMP_DIR}"
+}
+
+function extract_faces_from_videos() {
+
+  local target_dir=$1
+  local output_dir=$2
+
+  if [ -e "${output_dir}" ]; then
+    rm -rf "${output_dir}"
+  fi
+  mkdir "${output_dir}"
+
+  extract_images_from_videos "${target_dir}" "${TMP_IMAGES_DIR}"
+
+  python faceswap/faceswap.py extract \
+    -i "${TMP_IMAGES_DIR}" \
+    -o "${output_dir}"
+
+  rm -rf "${TMP_IMAGES_DIR}"
 }
