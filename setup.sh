9ae37aef687cae55f21c5a3396f03b79947dd43a
Suguru Imanaga <suguru_imanaga@anothervisions.net>
2020-04-23 10:47:33 +0000

    test

--- setup.sh
+++ setup.sh
@@ -2,14 +2,10 @@
 
 cd `dirname $0`
 
-# install anaconda
 wget -c https://repo.continuum.io/archive/Anaconda3-2019.10-Linux-x86_64.sh
 chmod +x Anaconda3-2019.10-Linux-x86_64.sh
-bash Anaconda3-2019.10-Linux-x86_64.sh -b -f -p /usr/local > /dev/null
+bash Anaconda3-2019.10-Linux-x86_64.sh -b -f -p /usr/local
 
-# install faceswap
-git clone https://github.com/deepfakes/faceswap.git > /dev/null
-conda install -y tk tensorflow-gpu=1.15 python=3.6 > /dev/null
-pip install -r faceswap/requirements.txt > /dev/null
-
-# update config
+git clone https://github.com/deepfakes/faceswap.git
+conda install -y tk tensorflow-gpu=1.15 python=3.6
+pip install -r faceswap/requirements.txt
